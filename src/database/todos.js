a19c0585641f23fbc5555edd2d42df3c209458ec
Kamila Lambert <kamila.p.lambert@gmail.com>
2017-05-16 13:29:45 -0700

    removeTodo function implemented

--- src/database/todos.js
+++ src/database/todos.js
@@ -24,21 +24,21 @@ function getAllTodos (req, res, next) {
 		})
 }
 
-function getOneTodo(req, res, next) {
-  let todoID = req.params.id;
-  db.one('select * from todos where id = $1', todoID)
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ONE todo'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
+// function getOneTodo(req, res, next) {
+//   let todoID = req.params.id;
+//   db.one('select * from todos where id = $1', todoID)
+//     .then(function (data) {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           data: data,
+//           message: 'Retrieved ONE todo'
+//         });
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
 
 function createTodo (req, res, next) {
 	db.none('INSERT INTO todos(to_do, complete)' + 'VALUES($1, $2)', [req.body.to_do, req.body.complete])
@@ -54,24 +54,24 @@ function createTodo (req, res, next) {
 		})
 }
 
-// let createTodo = (req, res, next) => {
-//   db.none('insert into todos(description, status, due)' + 'values($1, $2, $3)', [req.body.description, req.body.status, req.body.due])
-//     .then(() => {
-//       res.status(200)
-//       .json({
-//         status: 'success',
-//         message: 'inserted a todo'
-//       });
-//     })
-//     .catch(err => {
-//       return next(err);
-//     })
-// };
+function removeTodo (req, res, next) {
+	const todoID = parseInt(req.params.id)
+	db.result('DELETE FROM todos WHERE id = $1', todoID)
+		.then(function (result) {
+			res.status(200)
+				.json({
+					status: 'success',
+					message: `Remove ${result.rowCount} todo`
+				})
+		})
+		.catch(function (err) {
+			return next(err)
+		})
+}
 
 module.exports = {
 	getAllTodos: getAllTodos,
 	createTodo: createTodo,
-	getOneTodo: getOneTodo
-	// removeTodo: removeTodo,
+	removeTodo: removeTodo,
 	// editTodo: editTodo
 }
--- src/server/routes/todos.js
+++ src/server/routes/todos.js
@@ -4,12 +4,8 @@ const router = express.Router()
 const db = require('/Users/kamilalambert/Desktop/Projects/to-do-list/src/database/todos.js')
 
 router.get('/todos', db.getAllTodos)
-router.get('/todo/:id', db.getOneTodo)
 router.post('/todos', db.createTodo)
-// router.post('/todos', (req, res) => {
-// 	console.log(req.body)
-// })
 // router.put('/todos/:id', db.editTodo)
-// router.delete('/todos/:id', db.removeTodo)
+router.delete('/todos/:id', db.removeTodo)
 
 module.exports = router
